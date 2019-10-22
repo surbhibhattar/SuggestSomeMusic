eb055eb296606521a90314fb55fc61542f4e0e3b
Unknown <surbhibhattar92@gmail.com>
2019-10-23 00:18:17 +0530

    added iframe and ajax to fetch videos

--- javascript/home.js
+++ javascript/home.js
@@ -4,7 +4,8 @@ function remove_placeholder() {
 }
 
 function show_songs() {
-    location.href='suggestion.html';  
+    //location.href='suggestion.html';  
+    getVideo();
 }
  
 
@@ -36,3 +37,29 @@ function show_songs() {
   });
 
 
+  function getVideo() {
+    $.ajax({
+      type: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      data: {
+          key: 'AIzaSyAz3T3yM03y6ohshndj-vh5JTdztemtJ1A',
+          q: "cats",
+          part: 'snippet',
+          maxResults: 1,
+          type: 'video',
+          videoEmbeddable: true,
+      },
+      success: function(data){
+          embedVideo(data)
+      },
+      error: function(response){
+          console.log("Request Failed");
+      }
+    });
+  }
+
+  function embedVideo(data) {
+    $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+    $('h3').text(data.items[0].snippet.title)
+    $('.description').text(data.items[0].snippet.description)
+}
